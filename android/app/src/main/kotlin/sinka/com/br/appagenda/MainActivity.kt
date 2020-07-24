519f2a157340976b4dda1061ad5a9040d6579d62
andresnsn <59550765+andresnsn@users.noreply.github.com>
2020-07-24 16:28:03 -0300

    AppAgenda

---
+++ android/app/src/main/kotlin/sinka/com/br/appagenda/MainActivity.kt
@@ -0,0 +1,6 @@
+package sinka.com.br.appagenda
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:appagenda/UI/contact_page.dart';
+import 'package:flutter/material.dart';
+import 'package:appagenda/ui/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
